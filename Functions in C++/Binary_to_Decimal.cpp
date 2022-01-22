a1c2b11d739914c3226dee540ea0b8a92988fb3b
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-22 21:18:02 +0530

    Binary to Octal

---
+++ Functions in C++/Binary_to_Decimal.cpp
@@ -0,0 +1,28 @@
+// Binary to Decimal
+
+#include<iostream>
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+    int ans = 0;
+    int x = 1;
+    while(n > 0)
+    {
+        int y = n%10;
+        ans += x*y;
+        x *= 2;
+        n /= 10;
+    }
+    return ans;
+}
+
+int32_t main()
+{
+    int n;
+    cin>>n;
+
+    cout<<binaryToDecimal(n)<<endl;
+
+    return 0;
+}
