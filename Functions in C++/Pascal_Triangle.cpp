e102a2e9b11669bd93519d161b717e0e1a93e44f
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-22 20:04:53 +0530

    Pascal Triangle

---
+++ Functions in C++/Pascal_Triangle.cpp
@@ -0,0 +1,36 @@
+//Pascal Traingle  (iCj)
+
+//  1
+//  1 1
+//  1 2 1
+//  1 3 3 1
+//  1 4 6 4
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int f = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<=i; j++)
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
+
