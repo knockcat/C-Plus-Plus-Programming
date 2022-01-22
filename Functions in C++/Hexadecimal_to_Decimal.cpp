2d418571a2a04573fe6dfc7b4b7d44d5e6897884
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-22 21:30:34 +0530

    Hexadecimal To Decimal

---
+++ Functions in C++/Hexadecimal_to_Decimal.cpp
@@ -0,0 +1,36 @@
+// Hexadecimal to Decimal
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int HexadecimalToDecimal(string n)
+{
+    int ans = 0;
+    int x = 1;
+    int s = n.size();
+
+    for(int i = s-1; i>=0; i--)
+    {
+        if(n[i] >= '0' && n[i] <='9')
+        {
+            ans += x * (n[i] - '0') ;
+        }
+        else if(n[i] >= 'A' && n[i] <= 'F')
+        {
+            ans += x* (n[i] - 'A' + 10);
+        }
+        x *= 16;
+    }
+    return ans;
+}
+
+int32_t main()
+{
+    string n;
+    cin>>n;
+
+    cout<<HexadecimalToDecimal(n)<<endl;
+
+    return 0;
+}
