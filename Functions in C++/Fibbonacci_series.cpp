496eacae24b401883b2fad289ba1c38f41bd3729
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-22 19:45:08 +0530

    Fibbonacci Sequence

---
+++ Functions in C++/Fibbonacci_series.cpp
@@ -0,0 +1,28 @@
+// Fibonacci Sequence
+
+#include <iostream>
+using namespace std;
+
+void fib(int n)
+{
+    int t1 = 0;
+    int t2 = 1;
+    int next_term;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << t1 << endl;
+        next_term = t1 + t2;
+        t1 = t2;
+        t2 = next_term;
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    fib(n);
+    return 0;
+}
