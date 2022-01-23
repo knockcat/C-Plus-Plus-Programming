88201f0c9261cc2155aeb55c2139b33a6961b83b
VICKY <vishujoshi21nov2000@gmail.com>
2022-01-23 11:39:43 +0530

    Recursion

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,19 @@
+//Factorial of a Number
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if(n == 0)
+        return 1;
+    return n* fact(n-1);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fact(n)<<endl;
+    return 0;
+}
